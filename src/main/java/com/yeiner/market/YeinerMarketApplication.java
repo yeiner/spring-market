1c4b3c02ce1b27f2407db361739d99aa626d7bc0
Yeiner Andres Morales Gomez <yeimor@eltiempo.com>
2021-08-20 19:10:30 -0500

    Skeleton, config , entiti productos

---
+++ src/main/java/com/yeiner/market/YeinerMarketApplication.java
@@ -0,0 +1,13 @@
+package com.yeiner.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeinerMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeinerMarketApplication.class, args);
+	}
+
+}
