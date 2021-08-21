0cc2421dba9119a5777d7ad75bc8260ea9d2f019
Yeiner Andres Morales Gomez <yeiner.morales@gmail.com>
2021-08-21 16:34:35 -0500

    Service producto

--- src/main/java/com/yeiner/market/persistence/ProductoRepository.java
+++ src/main/java/com/yeiner/market/persistence/ProductoRepository.java
@@ -5,6 +5,7 @@ import com.yeiner.market.domain.repository.ProductRepository;
 import com.yeiner.market.persistence.crud.ProductoCrudRepository;
 import com.yeiner.market.persistence.entity.Producto;
 import com.yeiner.market.persistence.mapper.ProductMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.sound.midi.MidiChannel;
@@ -13,7 +14,10 @@ import java.util.Optional;
 
 @Repository
 public class ProductoRepository implements ProductRepository {
+    @Autowired
     private ProductoCrudRepository productoCrudRepository;
+
+    @Autowired
     private ProductMapper mapper;
 
     @Override
